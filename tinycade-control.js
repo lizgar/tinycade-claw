360e0385ac41098c9784a393fe772ddde16d30cf
Peter Gyory <petergyory@Peters-MacBook-Pro.local>
2022-03-22 18:12:31 -0600

    rebuild it all

--- tinycade-control.js
+++ tinycade-control.js
@@ -1,50 +1,104 @@
 // this is the js file to edit to change the control code
-const Beholder = window['beholder-detection'];
+const Beholder = window['beholder-detection'].default;
 let sliderMarker; // Global reference for our marker
 let leftM, rightM, shootM;
 
+let config = {
+  camera_params: {
+    videoSize: 1, // The video size values map to the following [320 x 240, 640 x 480, 1280 x 720, 1920 x 1080]
+    rearCamera: true, // Boolean value for defaulting to the rear facing camera. Only works on mobile
+    torch: true, // Boolean value for if torch/flashlight is on. Only works for rear facing mobile cameras. Can only be set from init
+  },
+  detection_params: {
+    minMarkerDistance: 2,
+    minMarkerPerimeter: 0.01,
+    maxMarkerPerimeter: 1,
+    sizeAfterPerspectiveRemoval: 49,
+    area: {
+      start: { x: 0.30, y: 0.14 },
+      end:   { x: 1, y: 0.55 },
+    },
+  },
+  feed_params: {
+    contrast: 0,
+    brightness: 0,
+    grayscale: 0,
+    flip: false,
+  },
+  overlay_params: {
+    present: true, // Determines if the Beholder overlay will display or be invisible entirely via display: none
+    hide: true, // Determines if the overlay should be hidden on the left of the screen or visible
+  },
+};
+
+let prevTime = Date.now();
+let dt, currTime;
+const UPDATE_RATE = 1000 / 30;
+let updateTime = UPDATE_RATE;
+let currx = 0;
+let refLeft, refRight;
 // Update function for the Beholder detection. This causes the markers to update, all input code should be done here
 function bUpdate() {
-  // This updates Beholder and makes sure the computer vision code is run every frame.
-  Beholder.update();
-  
-  // If marker 0 is present, lerp it's position from a pre-determined range to fit within 0-128 for pico 8
-  // if (sliderMarker.center.x !== NaN && sliderMarker.center.x !== 0 && pico8_gpio) {
-  //   let newTarget = (sliderMarker.center.x - 151) / (120) * 128;
-  //   currx = currx + (newTarget - currx) * 0.4;
-
-  //   // This sets data inside of the pico8_gpio array.
-  //   // This value can be read from within the cart with   PEEK(0x5f80)
-  //   pico8_gpio[0] = currx;
-
-  //   // If you want to push a value to another spot in pico 8 memory just use another index in the array
-  //   // You should only push numbers to this array :)
-  //   pico8_gpio[1] = 42
-
-  //   // To access this use PEEK(0x5f81), or PEEK(0x5f80 + 1) this can be usefull if you are looping through memory
-  // }
-
-  pico8_gpio[0] = leftM.present ? 1 : 0;
-  pico8_gpio[1] = rightM.present ? 1 : 0;
-  pico8_gpio[2] = shootM.present ? 0 : 1;
-
   // Queue up the next update iteration for the next frame
   requestAnimationFrame(bUpdate);
+
+  currTime = Date.now();
+  dt = currTime - prevTime;
+  prevTime = currTime;
+  
+  updateTime -= dt;
+  
+  if (updateTime > 0) return;
+  updateTime = UPDATE_RATE;
+  // This updates Beholder and makes sure the computer vision code is run every frame.
+  Beholder.update(); // 90 - 313
+  // console.log((sliderMarker.center.x - refLeft.center.x - 45) / 162) /// (refRight.center.x - refLeft.center.x);
+  
+  // If marker 0 is present, lerp it's position from a pre-determined range to fit within 0-128 for pico 8
+  if (sliderMarker.center.x !== NaN && sliderMarker.center.x !== 0 && pico8_gpio) {
+    let newTarget = (sliderMarker.center.x - refLeft.center.x - 45) / 162 * 128;
+    currx = currx + (newTarget - currx) * 0.4;
+
+    // This sets data inside of the pico8_gpio array.
+    // This value can be read from within the cart with   PEEK(0x5f80)
+    pico8_gpio[0] = currx;
+
+    // If you want to push a value to another spot in pico 8 memory just use another index in the array
+    // You should only push numbers to this array :)
+    // pico8_gpio[1] = 42;
+
+    // To access this use PEEK(0x5f81), or PEEK(0x5f80 + 1) this can be usefull if you are looping through memory
+  }
+
+  // pico8_gpio[0] = leftM.present ? 1 : 0;
+  // pico8_gpio[1] = rightM.present ? 1 : 0;
+  // pico8_gpio[0] = sliderMarker.center.x < 115 ? 1 : 0;
+  // pico8_gpio[1] = sliderMarker.center.x > 120 ? 1 : 0;
+  pico8_gpio[2] = shootM.present ? 0 : 1;
 }
 
 window.onload = () => {
   // Initialize the beholder detection library
+  // console.log('lol what');
 
   // This will not work on desktop, comment it out if you wish to test on desktop first for whatever reason
-  Beholder.init('#tinycade-div', { overlay_params: { present: true }, feed_params: { brightness: 0 }, camera_params: { rearCamera: true, torch: true, videoSize: 0 } });
+  Beholder.init('#tinycade-div', config);
 
   // For desktop testing only use this line
   // Beholder.init('#tinycade-div', { overlay_params: { present: true }, camera_params: { rearCamera: false, torch: false, videoSize: 0 } });
 
   // Get a reference to the marker with the id 0
   shootM = Beholder.getMarker(5);
-  leftM = Beholder.getMarker(4)
-  rightM = Beholder.getMarker(3);
-  leftM.timeout = 150
+  // leftM = Beholder.getMarker(4)
+  // rightM = Beholder.getMarker(3);
+  // leftM.timeout = 150
+  refLeft = Beholder.getMarker(0);
+  refLeft.timeout = 200;
+  refRight = Beholder.getMarker(1);
+  refRight.timeout = 200;
+  
+  sliderMarker = Beholder.getMarker(3);
+  sliderMarker.timeout = 200;
+  shootM.timeout = 120;
   bUpdate()
 }
